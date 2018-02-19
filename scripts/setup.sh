0a76bd59d7c1d8f83243ebbc5964186083ed28f6
Luke Melas <lukemelas@users.noreply.github.com>
2018-02-18 19:53:12 -0500

    Update setup

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -3,3 +3,16 @@ python -m spacy download en
 python -m spacy download de
 wget "https://raw.githubusercontent.com/moses-smt/mosesdecoder/master/scripts/generic/multi-bleu.perl"
 mv multi-bleu.perl scripts/
+
+# # Create smaller dataset for working
+# ls -la .data/iwslt/de-en
+# cp -r .data .data-big
+# ls -la .data/iwslt/de-en
+# head .data/iwslt/de-en/train.de-en.de
+# head .data/iwslt/de-en/train.de-en.en
+#  mv .data/iwslt/de-en/train.de-en.de .data/iwslt/de-en/train.de-en-full.de
+#  mv .data/iwslt/de-en/train.de-en.en .data/iwslt/de-en/train.de-en-full.en
+# head -10000 .data/iwslt/de-en/train.de-en-full.de > .data/iwslt/de-en/train.de-en.de
+# head -10000 .data/iwslt/de-en/train.de-en-full.en > .data/iwslt/de-en/train.de-en.en
+#  wc -l .data/iwslt/de-en/train.de-en.de
+#  wc -l .data/iwslt/de-en/train.de-en.en
